043d8a6c4d82b0522070288fb21d7472226e99ff
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-07-24 13:28:12 -0300

    Gerando parcelas mensais e suas datas

---
+++ src/estudosBasicos/DatasEmJava5.java
@@ -0,0 +1,25 @@
+package estudosBasicos;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatasEmJava5 {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Date dateInicial = new SimpleDateFormat("dd/MM/yyyy").parse("31/07/2021");/*Data iniciada*/
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dateInicial);
+		
+		for (int parcela = 1; parcela <= 12; parcela ++) {
+			calendar.add(Calendar.MONTH, 1);
+			
+			System.out.println("Parcela número: "+ parcela +" vencimento é em : "+ new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		}
+
+	}
+
+}
